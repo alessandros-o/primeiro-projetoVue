e06087097fffd8a1b1c95ec4fd0fcc54ee11cc64
Alessandro <olive.alessandros@gmail.com>
2022-03-17 18:32:25 -0300

    uploado de todos os arquivos

---
+++ src/components/ContadorGeral.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="contador">
+        <h2>Contador: {{ contador }}</h2>
+        <button @click="subtrair">-</button>
+        <button @click="somar">+</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'ContadorGeral',
+    data(){
+        return{
+            contador: 0
+        }
+    },
+    methods:{
+        somar(){
+            this.contador = this.contador + 1;
+        },
+        subtrair(){
+            this.contador = this.contador - 1;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TesteEvento.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="teste-evento">
+        <h1>Teste Evento</h1>
+        <p v-if="valida" @click="deixarMaiuscula">{{ mensagem }}</p>
+        <p v-else @click="deixarMinuscula">{{ mensagem }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TesteEvento',
+    data(){
+        return{
+            mensagem: 'Qualquer mensagem',
+            valida: true
+        }
+    },
+    methods:{
+        deixarMaiuscula(){
+            this.mensagem = this.mensagem.toUpperCase();
+            this.valida = false;
+        },
+        deixarMinuscula(){
+            this.mensagem = this.mensagem.toLowerCase();
+            this.valida = true;
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
